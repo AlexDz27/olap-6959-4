b6eb733a47b91c7d66fc3ca8350ab78f5bfbae71
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-10-01 20:06:21 +0300

    add base html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 function App() {
   return (
     <div className="cont">
-      <header className="header">Ferico test assignment</header>
-      <main className="main">qwewqe</main>
+      <header className="header">My tasks</header>
+      <main className="tasks-grid main">
+        <section className="tasks-grid__col tasks-grid__col--pending">
+          <p className="tasks-grid__col__title">Pending</p>
+        </section>
+        <section className="tasks-grid__col tasks-grid__col--in-progress">
+          <p className="tasks-grid__col__title">In progress</p>
+        </section>
+        <section className="tasks-grid__col tasks-grid__col--done">
+          <p className="tasks-grid__col__title">Done</p>
+        </section>
+      </main>
     </div>
   )
 }
