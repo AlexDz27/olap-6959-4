1a99257c8e020e6afd75a298026535abe0a15423
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-10-02 19:42:06 +0300

    w

---
+++ src/components/AddTask.tsx
@@ -0,0 +1,7 @@
+function AddTask() {
+  return (
+    <input className="add-task-input" placeholder="What needs to be done?" />
+  )
+}
+
+export default AddTask
--- src/components/TasksGrid.tsx
+++ src/components/TasksGrid.tsx
@@ -1,10 +1,12 @@
 import Task from './Task.tsx'
+import AddTask from './AddTask.tsx'
 
 function TasksGrid() {
   return (
     <main className="tasks-grid main">
       <section className="tasks-grid__col tasks-grid__col--pending">
         <p className="tasks-grid__col__title">Pending</p>
+        <AddTask />
         <ul className="tasks-list">
           <Task text="smth task 1 ts" />
           <li className="tasks-list__task">Smth task 2</li>
@@ -13,12 +15,14 @@ function TasksGrid() {
       </section>
       <section className="tasks-grid__col tasks-grid__col--in-progress">
         <p className="tasks-grid__col__title">In progress</p>
+        <AddTask />
         <ul className="tasks-list">
           <li className="tasks-list__task">Smth task 1</li>
         </ul>
       </section>
       <section className="tasks-grid__col tasks-grid__col--done">
         <p className="tasks-grid__col__title">Done</p>
+        <AddTask />
         <ul className="tasks-list">
           <li className="tasks-list__task tasks-list__task--done">Smth task 1</li>
         </ul>
