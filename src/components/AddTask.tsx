5f56206d55fed2435f353c1e36098aea78f8a7c7
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-10-02 20:35:30 +0300

    w

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,7 +1,17 @@
 function AddTask() {
   return (
-    <input className="add-task-input" placeholder="What needs to be done?" />
+    <input
+      className="add-task-input"
+      placeholder="What needs to be done?"
+      onKeyDown={handleAddingTask}
+    />
   )
+
+  function handleAddingTask(evt: React.KeyboardEvent<HTMLInputElement>) {
+    if (evt.key === 'Enter') {
+      console.log('enter pressed')
+    }
+  }
 }
 
 export default AddTask
