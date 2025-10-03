fa4940a165efc0bbc0b7c36815483ba3d66a3094
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-10-03 10:46:02 +0300

    w

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,23 +1,27 @@
+import { useState } from 'react'
+import '../assets/icons/enter-icon.svg'
+
 function AddTask() {
+  const [text, setText] = useState('')
+
   return (
-    <input
-      onKeyDown={handleAddingTask}
-      onChange={handleShowingEnterIcon}
-      
-      placeholder="What needs to be done?"
-      className="add-task-input"
-    />
+    <div className="add-task-cont">
+      <input
+        onKeyDown={handleAddingTask}
+        onChange={(evt) => setText(evt.target.value)}
+        
+        value={text}
+        placeholder="What needs to be done?"
+        className="add-task-input"
+      />
+
+      {text.length >= 1 && <span className="add-task__enter-icon">k</span>}
+    </div>
   )
 
   function handleAddingTask(evt: React.KeyboardEvent<HTMLInputElement>) {
     if (evt.key === 'Enter') {
       console.log('enter pressed. todo: handle adding task')
-    }
-  }
-
-  function handleShowingEnterIcon(evt: React.ChangeEvent<HTMLInputElement>) {
-    if (evt.target.value.length >= 1) {
-      console.log('more than one!!! should show enter icon')
     }
   }
 }
