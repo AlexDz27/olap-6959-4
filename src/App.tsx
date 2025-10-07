a6edaa99d52db6f2aaafc20ab31ef7664ac3f080
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-10-07 16:07:03 +0300

    w

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
+import { useState, useEffect } from 'react'
 import Header from './components/Header.tsx'
 import TasksGrid from './components/TasksGrid.tsx'
+// TODO: r)
 import type { TaskI } from './components/Task.tsx'
 
-const tasks: TaskI[] = [
-  {
-    id: 1,
-    title: 'smth task 1 ts (prop)',
-    status: 'pending',
-  },
-  {
-    id: 2,
-    title: 'smth task 2 ts (prop)',
-    status: 'inProgress',
-  },
-  {
-    id: 3,
-    title: 'smth task 3 ts (done) (prop)',
-    status: 'done',
-  },
-]
+const ERR_MSG_TEXT = 'Oops, sorry! Unexpected error occrured when trying to load your tasks.'
+
+// TODO: r
+// const tasks: TaskI[] = [
+//   {
+//     id: 1,
+//     title: 'smth task 1 ts (prop)',
+//     status: 'pending',
+//   },
+//   {
+//     id: 2,
+//     title: 'smth task 2 ts (prop)',
+//     status: 'inProgress',
+//   },
+//   {
+//     id: 3,
+//     title: 'smth task 3 ts (done) (prop)',
+//     status: 'done',
+//   },
+// ]
 
 function App() {
+  const [tasks, setTasks] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [loadingError, setLoadingError] = useState(false)
+
+  useEffect(() => {
+    fetch('http://localhost:8088')
+      .then(response => {
+        if (!response.ok) throw new Error('Received bad code response from server')
+        return response.json()
+      })
+      .then(response => {
+        console.log(response)  // Leave console.logging intentionally for the reviewer of the app to be able to look into response
+        setTasks(response)
+      })
+      .catch(err => {
+        setLoadingError(true)
+        console.error(err)
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  if (loading) return <div className="cont loading-message">Loading tasks...</div>
+  if (loadingError) return <div className="cont loading-message">{ERR_MSG_TEXT}</div>
+
   return (
     <div className="cont">
       <Header />
--- src/components/TasksGrid.tsx
+++ src/components/TasksGrid.tsx
@@ -7,29 +7,46 @@ interface TasksGridProps {
 }
 
 function TasksGrid({ tasks }: TasksGridProps) {
+  const tasksSorted = sortTasks(tasks)
+
   return (
     <main className="tasks-grid main">
       <section className="tasks-grid__col tasks-grid__col--pending">
         <p className="tasks-grid__col__title">Pending</p>
         <AddTask />
         <ul className="tasks-list">
-          <Task task={tasks[0]} />
+          {tasksSorted.pending.length === 0 && <li className="no-tasks-yet">No pending tasks yet</li>}
+          {tasksSorted.pending.map(t => <Task task={t} key={t.id} />)}
         </ul>
       </section>
+
       <section className="tasks-grid__col tasks-grid__col--in-progress">
         <p className="tasks-grid__col__title">In progress</p>
         <ul className="tasks-list">
-          <Task task={tasks[1]} />
+          {tasksSorted.inProgress.length === 0 && <li className="no-tasks-yet">No in progress tasks yet</li>}
+          {tasksSorted.inProgress.map(t => <Task task={t} key={t.id} />)}
         </ul>
       </section>
+      
       <section className="tasks-grid__col tasks-grid__col--done">
         <p className="tasks-grid__col__title">Done</p>
         <ul className="tasks-list">
-          <Task task={tasks[2]} />
+          {tasksSorted.done.length === 0 && <li className="no-tasks-yet">No done tasks yet</li>}
+          {tasksSorted.done.map(t => <Task task={t} key={t.id} />)}
         </ul>
       </section>
     </main>
   )
+}
+
+function sortTasks(tasks: TaskI[]) {
+  const tasksSorted = {
+    pending: tasks.filter(t => t.status === 'pending'),
+    inProgress: tasks.filter(t => t.status === 'inProgress'),
+    done: tasks.filter(t => t.status === 'done')
+  }
+
+  return tasksSorted
 }
 
 export default TasksGrid
